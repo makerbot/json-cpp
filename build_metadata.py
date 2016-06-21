56df92160904165a9658a06cda11ff96eb0a5a62
Chris Moore <chris.moore@makerbot.com>
2016-06-21 10:18:54 -0400

    Update required TR version
    Need those cross compile changes to compile at all...

--- build_metadata.py
+++ build_metadata.py
@@ -8,6 +8,6 @@ else:
         artifactory_utils.ArtifactSelector(
             project="Toolchain-Release",
             revision="master",
-            version="3.9.*",
+            version="3.10.*",
             stable_required=True)
     ]
